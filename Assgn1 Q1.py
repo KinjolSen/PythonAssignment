c079d3f7f0129580c638eb934ef94260caa2708a
KinjolSen <95646826+KinjolSen@users.noreply.github.com>
2021-12-19 19:25:19 +0530

    Add files via upload

---
+++ Assgn1 Q1.py
@@ -0,0 +1,19 @@
+#Assignment 1
+
+#Question 1
+#Algorithm
+
+# 1. Start
+# 2. Get n, n is the previous number
+# 3. x=n-2 and y=n+2
+# 4. Stop
+
+n={"10", "1", "8", "3", "6", "5", "4", "7", "x", "y"}
+n=4
+x=n-2
+print(x)
+n=7
+y=n+2
+print(y)
+
+
