7cc07b8ef31c042e36d4f12bb238d6a9db88d4dd
KinjolSen <95646826+KinjolSen@users.noreply.github.com>
2021-12-19 19:26:01 +0530

    Add files via upload

---
+++ Assignment 1.1.py
@@ -0,0 +1,19 @@
+#Assignment 1.1
+#Question 1 and 2
+
+x=6
+print(x>3 and x<11)
+
+print(x>3 or x<4)
+
+print(not(x>3 and x<11))
+
+#Question 2
+a=2
+b=1
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
